0bb499d2bc3d8fbf27c4af6baae34a5d1af8f61e
nikita-dmatrix <156250739+nikita-dmatrix@users.noreply.github.com>
2025-05-15 16:40:33 +0100

    Update evaluator.py

--- lm_eval/evaluator.py
+++ lm_eval/evaluator.py
@@ -34,6 +34,7 @@ from lm_eval.utils import (
     setup_logging,
     simple_parse_args_string,
 )
+from .llama_sbfp import wrap_model, wrap_model_weights
 
 
 if TYPE_CHECKING:
@@ -323,6 +324,14 @@ def simple_evaluate(
 
     if check_integrity:
         run_task_tests(task_list=tasks)
+
+    print("I'm here!!!!!")
+    print(type(lm))
+    print(type(lm.model))
+    print(type(lm.model.model.layers[0].self_attn))
+    #from .opt_hack_sbfp_8 import wrap_model, wrap_model_weights
+    wrap_model_weights(lm.model)
+    #wrap_model(lm.model, quantize=True, block_size=16)
 
     if evaluation_tracker is not None:
         evaluation_tracker.general_config_tracker.log_experiment_args(
